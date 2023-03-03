ceb57178dfcae7edae83ffe11a0b7a1bb73ec9a7
Danil <danilkaspirin@gmail.com>
2023-03-03 22:19:34 +1000

    Init commit

---
+++ src/main/java/ru/danilspirin/publishingcompany/PublishingCompanyApplication.java
@@ -0,0 +1,13 @@
+package ru.danilspirin.publishingcompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublishingCompanyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PublishingCompanyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/danilspirin/publishingcompany/PublishingCompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.danilspirin.publishingcompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublishingCompanyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
