d138f3afa26d62b56e968af79385a64c3bccc52c
Danil <danilkaspirin@gmail.com>
2023-03-09 17:08:17 +1000

    feat: add Fullname class

---
+++ src/main/java/ru/danilspirin/publishingcompany/models/FullName.java
@@ -0,0 +1,14 @@
+package ru.danilspirin.publishingcompany.models;
+
+import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.FieldDefaults;
+
+@Embeddable
+@Setter @Getter @ToString @FieldDefaults(level = AccessLevel.PRIVATE)
+public class FullName {
+    String name, surname, patronymic;
+}
