4d72cdaa3c29ff8bdeadaff2f408df9378e3560a
Danil <danilkaspirin@gmail.com>
2023-03-14 19:25:34 +1000

    refactor: change errors message

--- src/main/java/ru/danilspirin/publishingcompany/exceptions/EntityAlreadyExistsException.java
+++ src/main/java/ru/danilspirin/publishingcompany/exceptions/EntityAlreadyExistsException.java
@@ -2,7 +2,7 @@ package ru.danilspirin.publishingcompany.exceptions;
 
 public class EntityAlreadyExistsException extends IllegalArgumentException{
     public EntityAlreadyExistsException(String id, Class<?> entityClass){
-        super(String.format("Сущность типа %s с id %s уже существует. " +
+        super(String.format("Сущность типа %s с id \"%s\" уже существует. " +
                 "Укажите id не существующей сущности", //TODO: i18n
                 entityClass.getName(),
                 id));
--- src/main/java/ru/danilspirin/publishingcompany/exceptions/EntityWithIdIsNotExistsException.java
+++ src/main/java/ru/danilspirin/publishingcompany/exceptions/EntityWithIdIsNotExistsException.java
@@ -3,7 +3,7 @@ package ru.danilspirin.publishingcompany.exceptions;
 public class EntityWithIdIsNotExistsException extends IllegalArgumentException{
     public EntityWithIdIsNotExistsException(String wrongId, Class<?> entityClass){
         super(String.format(
-                "Сущность типа %s с id %s не существует." +
+                "Сущность типа %s с id \"%s\" не существует." +
                         " Укажите id существующей сущности.",   //TODO: i18n
                 entityClass.getName(),
                 wrongId)
